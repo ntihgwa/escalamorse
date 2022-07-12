0da19413e7a7a50ad9808b90ab21045c89e9ad0a
ntihgwa <109122409+ntihgwa@users.noreply.github.com>
2022-07-11 23:41:52 -0300

    Add files via upload
    Criação da Index, csss e funções Java script.

---
+++ scripts.js
@@ -0,0 +1,117 @@
+var answers = {};
+
+var question_one = document.getElementById('question-1');
+var question_two = document.getElementById('question-2');
+var question_three = document.getElementById('question-3');
+var question_four = document.getElementById('question-4');
+var question_five = document.getElementById('question-5');
+var question_six = document.getElementById('question-6');
+
+function storeAnswer(question_number, event){
+    if(event.target.type === 'radio'){
+        console.log(event.target.value);
+        answers['question'+question_number] = parseInt(event.target.value);
+        console.log(answers);
+    }
+}
+
+question_one.addEventListener('click', function(event){
+    storeAnswer(1, event)
+})
+question_two.addEventListener('click', function(event){
+    storeAnswer(2, event)
+})
+question_three.addEventListener('click', function(event){
+    storeAnswer(3, event)
+})
+question_four.addEventListener('click', function(event){
+    storeAnswer(4, event)
+})
+question_five.addEventListener('click', function(event){
+    storeAnswer(5, event)
+})
+question_six.addEventListener('click', function(event){
+    storeAnswer(6, event)
+})
+
+function totalScore(){
+    var total_score = 
+    answers.question1+
+    answers.question2+
+    answers.question3+
+    answers.question4+ 
+    answers.question5+
+    answers.question6;
+    
+    return total_score;
+}
+
+function getInfoBasedOnScore(){
+    if(totalScore() < 24){
+        var score_info = "RISCO LEVE DE QUEDAS";
+    } else if(totalScore() >= 25){
+        var score_info = "RISCO MODERADO DE QUEDAS!";
+    }
+
+    return score_info;
+}
+
+var submit1 = document.getElementById('submit1');
+var submit2 = document.getElementById('submit2');
+var submit3 = document.getElementById('submit3');
+var submit4 = document.getElementById('submit4');
+var submit5 = document.getElementById('submit5');
+var submit6 = document.getElementById('submit6');
+
+function nextQuestion(question_number){
+    var current_question_number = question_number - 1;
+    var question_number = question_number.toString();
+    var current_question_number = current_question_number.toString();
+
+    var el = document.getElementById('question-'+question_number);
+    var el2 = document.getElementById('question-'+current_question_number);
+
+    el.style.display = "block";
+    el2.style.display = "none";
+}
+
+submit1.addEventListener('click', function(){
+    nextQuestion(2);
+    growProgressBar('20%');
+})
+submit2.addEventListener('click', function(){
+    nextQuestion(3);
+    growProgressBar('40%');
+})
+submit3.addEventListener('click', function(){
+    nextQuestion(4);
+    growProgressBar('60%');
+})
+submit4.addEventListener('click', function(){
+    nextQuestion(5);
+    growProgressBar('80%');
+})
+submit5.addEventListener('click', function(){
+    nextQuestion(6);
+    growProgressBar('100%');
+})
+submit6.addEventListener('click', function(){
+    nextQuestion(7);
+})
+
+submit6.addEventListener('click', function(){
+    document.getElementById("printtotalscore").innerHTML = totalScore();
+    document.getElementById("printscoreinfo").innerHTML = getInfoBasedOnScore();
+})
+
+function growProgressBar(percentage_width){
+    var bar = document.getElementById("progress_bar");
+    bar.style.width = percentage_width;
+}
+
+document.querySelector("form").addEventListener("click", (e)=>{
+    if(!document.querySelector("input[name='favelang']").checked){
+       alert("Marque uma opção!");
+       e.preventDefault();
+    }
+ });
